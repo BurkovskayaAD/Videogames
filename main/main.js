7c940b120d028a9607ce0df42f08249499faa2d4
Ilya Nogach <ilanogac284@gmail.com>
2026-03-23 20:11:55 +0300

    add list of genres

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
 let root = document.getElementById("root");
+
+const urlGenres = "https://api.rawg.io/api/genres?key=0fc5072e9d5f45a29c5718c6d74bcf2a"
+let genresList = [];
+async function fetchGenres() {
+        const response = await fetch(urlGenres);
+        const data = await response.json();
+        genresList = data.results;
+        console.log(genresList)
+        createSelect();
+}
+function createSelect(){
+    const formSelect = document.querySelector(".header-form_search");
+    const allOption = new Option('All genres', '');
+    formSelect.add(allOption);
+    genresList.forEach((genre) => {
+    let option = new Option(genre.name, genre.id);
+    formSelect.add(option);
+    });
+}
+
 function createHeader(){
     let header = document.createElement('div')
     root.append(header);
@@ -11,10 +31,6 @@ function createHeader(){
     formInputSearch.classList.add("header-form_inputSearch");
     formInputSearch.placeholder = "Search"
     let formSelect = document.createElement("select");
-        ["All", "fantasy", "horror"].forEach((text, index) => {
-    let option = new Option(text, "value" + index);
-    formSelect.add(option);
-    });
     form.append(formSelect);
     formSelect.name = "genres";
     formSelect.classList.add("header-form_search");
@@ -25,9 +41,13 @@ function createHeader(){
     buttonSearch.textContent ="SEARCH";
     buttonSearch.addEventListener("click", function(event){
         event.preventDefault();
-
     })
+    if (genresList.length > 0) {
+        createSelect();
+    }
 }
-
+fetchGenres();
 createHeader();
+
+
 
